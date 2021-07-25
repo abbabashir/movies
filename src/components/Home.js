6e031f113c2792695e54a60b1bee4c598c3afbd6
abbabashir <abbabashir516@gmail.com>
2021-07-25 02:20:28 -0700

    --- Auto-Git Commit ---

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import API from '../API';
 
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASEURL } from '../config';
 
@@ -8,6 +9,29 @@ const Home = () => {
     const [state, setState] = useState();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+
+    const fetchMovies = async (page, searchTerm = "") => {
+        try{
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+
+            setState(prev => ({
+                ...movies,
+                results:
+                page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }));
+        }catch(error){
+            setError(true);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchMovies(1)
+    },[])
+    console.log(state);
 
     return <div>Home Page</div>
 };
