9b85241345bcb17bfe420a3bf2622efb6cdae10e
abbabashir <abbabashir516@gmail.com>
2021-07-25 14:40:55 -0700

    --- Auto-Git Commit ---

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,55 @@ import React, { useState, useEffect } from 'react';
 import API from '../API';
 
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
-import { useHomeFetch } from '../hooks/useHomeFetch';
 import HeroImage from './HeroImage';
 import NOIMage from '../images/no_image.jpg';
 
-const Home = () => {
-    const { state, loading, error } = useHomeFetch();
-    console.log(state);
 
+const initialState = {
+        page: 0,
+        results: [],
+        total_pages: 0,
+        total_results: 0
+    }
+
+
+
+const Home = () => {
+    const [state, setState] = useState(initialState);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+
+            setState(prev => ({
+                ...movies,
+                results: 
+                page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }));
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    }
+
+    useEffect(() => {
+
+        fetchMovies(1)
+    }, []);
     return (
         <>
-            {state.results[0] ? (
-                <HeroImage
-                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-                    title={state.results[0].original_title}
-                    text={state.results[0].overview}
-                />
-            ) : null}
+        {state.results[0] ? (
+            <HeroImage
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+            />
+        ) : null}
         </>
     );
 };
