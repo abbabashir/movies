92d69a7bca29e8c4743ab852c62358eb04d56dcb
abbabashir <abbabashir516@gmail.com>
2021-07-25 15:44:57 -0700

    --- Auto-Git Commit ---

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-
-
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
 
-import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
+
 
 const Header = () => (
     <Wrapper>
