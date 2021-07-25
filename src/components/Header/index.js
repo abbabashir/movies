6ea6ef832b98478224c397282e823f85c51496ff
abbabashir <abbabashir516@gmail.com>
2021-07-25 16:15:33 -0700

    --- Auto-Git Commit ---

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,45 @@ import React from 'react';
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
 
+import styled from 'styled-components';
+
+
+const Wrapper = styled.div`
+  background: var(--darkGrey);
+  padding: 0 20px;
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: var(--maxWidth);
+  padding: 20px 0;
+  margin: 0 auto;
+`;
+
+const LogoImg = styled.img`
+  width: 200px;
+
+  @media screen and (max-width: 500px) {
+    width: 150px;
+  }
+`;
+
+const TMDBLogoImg = styled.img`
+  width: 100px;
+
+  @media screen and (max-width: 500px) {
+    width: 80px;
+  }
+`;
 
 
 const Header = () => (
     <Wrapper>
         <Content>
-            <LogoImg src={RMDBLogo} alt='rmdb-logo' />
-            <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
+            <LogoImg src={RMDBLogo.svg} />
+            <TMDBLogoImg src={TMDBLogo.svg} />
         </Content>
     </Wrapper>
 );
