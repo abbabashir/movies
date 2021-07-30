598f476052927922a58b896b4f7a9c5d1e745652
abbabashir <abbabashir516@gmail.com>
2021-07-30 07:08:29 -0700

    Got Movie data from api, added routes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+import Grid from './Grid';
+import Spinner from './Spinner';
+import { useMovieFetch } from '../hooks/useMovieFetch';
+import NoImage from '../images/no_image.jpg';
 
-const Movie = () => <div>Movie</div>
+const Movie = () => {
+    const { movieId } = useParams();
+
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+
+    console.log(movie);
+    return(
+        <>
+            <div>Movies</div>
+        </>
+    )
+}
 
 export default Movie;
